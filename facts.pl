7b72fd41a0fc9923345721e741b086391e16fdee
Feras AL-Kassar <en.feras@hotmail.com>
2016-10-12 00:09:18 +0200

    add the first part of the game and edit some functions

--- facts.pl
+++ facts.pl
@@ -1,3 +1,11 @@
+% e empty position
+% a first player
+% b second player
+
+%Change between players
+another_player(a,b).
+another_player(b,a).
+
 % Positions
 % the first row
 position(1,1).
@@ -39,8 +47,8 @@ position(7,7).
 
 %convert from two dimentions to one dimention
 dim(1,1,1).
-dim(1,2,2).
-dim(1,3,3).
+dim(1,4,2).
+dim(1,7,3).
 dim(2,2,4).
 dim(2,4,5).
 dim(2,6,6).
@@ -63,55 +71,55 @@ dim(7,1,22).
 dim(7,4,23).
 dim(7,7,24).
 
-%Check if the player has new score or not
-score(Board,X,Y,T):- X=:=1, Y=:=1, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=1, Y=:=1, Board = [T,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_].
-score(Board,X,Y,T):- X=:=1, Y=:=4, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=1, Y=:=4, Board = [_,T,_,_,T,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=1, Y=:=7, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=1, Y=:=7, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
-score(Board,X,Y,T):- X=:=2, Y=:=2, Board = [_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=2, Y=:=2, Board = [_,_,_,T,_,_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=2, Y=:=4, Board = [_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=2, Y=:=4, Board = [_,T,_,_,T,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=2, Y=:=6, Board = [_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=2, Y=:=6, Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_,_,T,_,_,_].
-score(Board,X,Y,T):- X=:=3, Y=:=3, Board = [_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=3, Y=:=3, Board = [_,_,_,_,_,_,T,_,_,_,_,T,_,_,_,T,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=3, Y=:=4, Board = [_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=3, Y=:=4, Board = [_,T,_,_,T,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=3, Y=:=5, Board = [_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=3, Y=:=5, Board = [_,_,_,_,_,_,_,_,T,_,_,_,T,_,_,_,_,T,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=1, Board = [_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=1, Board = [T,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=2, Board = [_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=2, Board = [_,_,_,T,_,_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=3, Board = [_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=3, Board = [_,_,_,_,_,_,T,_,_,_,_,T,_,_,_,T,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=5, Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=5, Board = [_,_,_,_,_,_,_,_,T,_,_,_,T,_,_,_,_,T,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=6, Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_,_,T,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=6, Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=7, Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=4, Y=:=7, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
-score(Board,X,Y,T):- X=:=5, Y=:=3, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=5, Y=:=3, Board = [_,_,_,_,_,_,T,_,_,_,_,T,_,_,_,T,_,_,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=5, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=5, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,T,_,_,T,_].
-score(Board,X,Y,T):- X=:=5, Y=:=5, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=5, Y=:=5, Board = [_,_,_,_,_,_,_,_,T,_,_,_,T,_,_,_,_,T,_,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=6, Y=:=2, Board = [_,_,_,T,_,_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_].
-score(Board,X,Y,T):- X=:=6, Y=:=2, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_].
-score(Board,X,Y,T):- X=:=6, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_].
-score(Board,X,Y,T):- X=:=6, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,T,_,_,T,_].
-score(Board,X,Y,T):- X=:=6, Y=:=6, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_].
-score(Board,X,Y,T):- X=:=6, Y=:=6, Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_,_,T,_,_,_].
-score(Board,X,Y,T):- X=:=7, Y=:=1, Board = [T,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_].
-score(Board,X,Y,T):- X=:=7, Y=:=1, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
-score(Board,X,Y,T):- X=:=7, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
-score(Board,X,Y,T):- X=:=7, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,T,_,_,T,_].
-score(Board,X,Y,T):- X=:=7, Y=:=7, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
-score(Board,X,Y,T):- X=:=7, Y=:=7, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
+%Check if the player has new triple or not and check if there are triple so the player cannot delete antone of them.
+triple(Board,X,T):- X=:=1, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=1, Board = [T,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_].
+triple(Board,X,T):- X=:=2, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=2, Board = [_,T,_,_,T,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=3, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=3, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
+triple(Board,X,T):- X=:=4, Board = [_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=4, Board = [_,_,_,T,_,_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_].
+triple(Board,X,T):- X=:=5, Board = [_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=5, Board = [_,T,_,_,T,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=6, Board = [_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=6, Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_,_,T,_,_,_].
+triple(Board,X,T):- X=:=7, Board = [_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=7, Board = [_,_,_,_,_,_,T,_,_,_,_,T,_,_,_,T,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=8, Board = [_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=8, Board = [_,T,_,_,T,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=9, Board = [_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=9, Board = [_,_,_,_,_,_,_,_,T,_,_,_,T,_,_,_,_,T,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=10, Board = [_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=10, Board = [T,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_].
+triple(Board,X,T):- X=:=11, Board = [_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=11, Board = [_,_,_,T,_,_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_].
+triple(Board,X,T):- X=:=12, Board = [_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=12, Board = [_,_,_,_,_,_,T,_,_,_,_,T,_,_,_,T,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=13, Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=13, Board = [_,_,_,_,_,_,_,_,T,_,_,_,T,_,_,_,_,T,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=14, Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_,_,T,_,_,_].
+triple(Board,X,T):- X=:=14, Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=15, Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=15, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
+triple(Board,X,T):- X=:=16, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=16, Board = [_,_,_,_,_,_,T,_,_,_,_,T,_,_,_,T,_,_,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=17, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=17, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,T,_,_,T,_].
+triple(Board,X,T):- X=:=18, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=18, Board = [_,_,_,_,_,_,_,_,T,_,_,_,T,_,_,_,_,T,_,_,_,_,_,_].
+triple(Board,X,T):- X=:=19, Board = [_,_,_,T,_,_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_].
+triple(Board,X,T):- X=:=19, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_].
+triple(Board,X,T):- X=:=20, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_].
+triple(Board,X,T):- X=:=20, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,T,_,_,T,_].
+triple(Board,X,T):- X=:=21, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T,_,_,_].
+triple(Board,X,T):- X=:=21, Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,T,_,_,_,_,_,_,T,_,_,_].
+triple(Board,X,T):- X=:=22, Board = [T,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_].
+triple(Board,X,T):- X=:=22, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
+triple(Board,X,T):- X=:=23, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
+triple(Board,X,T):- X=:=23, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,T,_,_,T,_].
+triple(Board,X,T):- X=:=24, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
+triple(Board,X,T):- X=:=24, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
 
 %check if the player can make a move ot not
 can_move(Board,T):- Board = [T,e,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
@@ -179,53 +187,112 @@ can_move(Board,T):- Board = [_,_,e,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_].
 can_move(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,e].
 can_move(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,e,_,_,_,_,_,_,_,_,T].
 
+%check if there any piece to delete it or not.
+can_delete(Board,T):- Board = [T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,1,T).
+can_delete(Board,T):- Board = [_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,2,T).
+can_delete(Board,T):- Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,3,T).
+can_delete(Board,T):- Board = [_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,4,T).
+can_delete(Board,T):- Board = [_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,5,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,6,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,7,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,8,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,9,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,10,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,11,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,12,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,13,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_,_],\+ triple(Board,14,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,_],\+ triple(Board,15,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_],\+ triple(Board,16,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_],\+ triple(Board,17,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_],\+ triple(Board,18,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_],\+ triple(Board,19,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_],\+ triple(Board,20,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_],\+ triple(Board,21,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,_],\+ triple(Board,22,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_],\+ triple(Board,23,T).
+can_delete(Board,T):- Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T],\+ triple(Board,24,T).
+
 %Print the Board
 /*
-O--------O--------O
-|        |        |
-|  O-----O-----O  |
-|  |     |     |  |
-|  |  O--O--O  |  |
-|  |  |     |  |  |
-O--O--O     O--O--O
-|  |  |     |  |  |
-|  |  O--O--O  |  |
-|  |     |     |  |
-|  O-----O-----O  |
-|        |        |
-O--------O--------O
+  1  2  3  4  5  6  7
+1 O--------O--------O
+  |        |        |
+2 |  O-----O-----O  |
+  |  |     |     |  |
+3 |  |  O--O--O  |  |
+  |  |  |     |  |  |
+4 O--O--O     O--O--O
+  |  |  |     |  |  |
+5 |  |  O--O--O  |  |
+  |  |     |     |  |
+6 |  O-----O-----O  |
+  |        |        |
+7 O--------O--------O
 */
 print_element([X|Y],A,N):- A=:=N,write(X).
 print_element([X|Y],A,N):- print_element(Y,A+1,N).
 print_board(Board):- 
-	print_element(Board,1,1),write('--------'),print_element(Board,1,2),write('--------'),print_element(Board,1,3),nl, 
-	write('|        |        |'),nl,
-	write('|  '),print_element(Board,1,4),write('-----'),print_element(Board,1,5),write('-----'),print_element(Board,1,6),write('  |'),nl,
-	write('|  |     |     |  |'),nl,
-	write('|  |  '),print_element(Board,1,7),write('--'),print_element(Board,1,8),write('--'),print_element(Board,1,9),write('  |  |'),nl,
-	write('|  |  |     |  |  |'),nl,
-	print_element(Board,1,10),write('--'),print_element(Board,1,11),write('--'),print_element(Board,1,12),write('     '),print_element(Board,1,13),write('--'),print_element(Board,1,14),write('--'),print_element(Board,1,15),nl,
-	write('|  |  |     |  |  |'),nl,
-	write('|  |  '),print_element(Board,1,16),write('--'),print_element(Board,1,17),write('--'),print_element(Board,1,18),write('  |  |'),nl,
-	write('|  |     |     |  |'),nl,
-	write('|  '),print_element(Board,1,19),write('-----'),print_element(Board,1,20),write('-----'),print_element(Board,1,21),write('  |'),nl,
-	write('|        |        |'),nl,
-	print_element(Board,1,22),write('--------'),print_element(Board,1,23),write('--------'),print_element(Board,1,24),nl.
+	write('  1  2  3  4  5  6  7'),nl,
+	write('1 '),print_element(Board,1,1),write('--------'),print_element(Board,1,2),write('--------'),print_element(Board,1,3),nl, 
+	write('  |        |        |'),nl,
+	write('2 |  '),print_element(Board,1,4),write('-----'),print_element(Board,1,5),write('-----'),print_element(Board,1,6),write('  |'),nl,
+	write('  |  |     |     |  |'),nl,
+	write('3 |  |  '),print_element(Board,1,7),write('--'),print_element(Board,1,8),write('--'),print_element(Board,1,9),write('  |  |'),nl,
+	write('  |  |  |     |  |  |'),nl,
+	write('4 '),print_element(Board,1,10),write('--'),print_element(Board,1,11),write('--'),print_element(Board,1,12),write('     '),
+		print_element(Board,1,13),write('--'),print_element(Board,1,14),write('--'),print_element(Board,1,15),nl,
+	write('  |  |  |     |  |  |'),nl,
+	write('5 |  |  '),print_element(Board,1,16),write('--'),print_element(Board,1,17),write('--'),print_element(Board,1,18),write('  |  |'),nl,
+	write('  |  |     |     |  |'),nl,
+	write('6 |  '),print_element(Board,1,19),write('-----'),print_element(Board,1,20),write('-----'),print_element(Board,1,21),write('  |'),nl,
+	write('  |        |        |'),nl,
+	write('7 '),print_element(Board,1,22),write('--------'),print_element(Board,1,23),write('--------'),print_element(Board,1,24),nl.
 
-%print_board(['O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O']).
+%print_board(['e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e']).
 
 %check if the position in empty or not
-available_position([X|Y],A,N):- A=:=N,X=:='e'.
-available_position([X|Y],A,N):- A=\=N,available_position(Y,A+1,N).
+available_position(['e'|_],1).
+available_position([_|Y],N):- N>0 ,N1 is N-1,available_position(Y,N1).
 
-%check if the move is correct or not
-check(Board,X,Y,T):-position(X,Y),dim(X,Y,Z),write(Z),available_position(Board,1,Z).
-check(Board,X,Y,T):-write('Incorrect, this position is not available').
+%check if the position in empty or not
+position_has_Tpiece([T|_],1,T).
+position_has_Tpiece([_|Y],N,T):- N>0 ,N1 is N-1,position_has_Tpiece(Y,N1,T).
+
+%check if the move (add piece) is correct or not
+check_add_piece(Board,X,Y,T):-position(X,Y),dim(X,Y,Z),available_position(Board,Z).
+
+%add piece to the Board
+add_piece([_|L],[P|L],P,1).
+add_piece([X|L],[X|L1],P,N):-N1 is N-1,add_piece(L,L1,P,N1).
+
+%remove piece from the Board
+remove_piece([_|L],['e'|L],1).
+remove_piece([X|L],[X|L1],N):-N1 is N-1,remove_piece(L,L1,N1).
+
+%check if the move (delete piece) is correct or not
+check_delete_piece(Board,X,Y,T):- position(X,Y),dim(X,Y,Z),position_has_Tpiece(Board,Z,T),\+ triple(Board,Z,T).
+
+%delete one piece from the against player when the player have new triple
+delete_piece(Board,T,Board):- another_player(T,T1),\+ can_delete(Board,T1),write('you have new triple but you cannot delete any piece.'),nl.
+delete_piece(Board,T,NewBoard):- write('You have new triple so you can choose one piece from the other pieces to delete it'),nl,
+	write('Enter the number of row: '),nl,read(X),
+	write('Enter the number of column: '),nl,read(Y),
+	another_player(T,T1),check_delete_piece(Board,X,Y,T1),dim(X,Y,Z),remove_piece(Board,NewBoard,Z).
+delete_piece(Board,T,NewBoard):- write('Incorrect, this move is not available'),nl,delete_piece(Board,T,NewBoard).
+
+%check if the player has triple
+check_triple(Board,X,T,NewBoard):- triple(Board,X,T),delete_piece(Board,T,NewBoard).
+check_triple(L,_,_,L).
+
 
 %playing the game
-play(Board,T):- write('Enter the number of row: '),nl,read(X),
+play(Board,T):- nl,print_board(Board),nl,
+	write('***** Player '),write(T),write(' *****'),nl,
+	write('Enter the number of row: '),nl,read(X),
 	write('Enter the number of column: '),nl,read(Y),
-	check(Board,X,Y,T).
-play(Board,T):- play(Board,T).
+	check_add_piece(Board,X,Y,T),dim(X,Y,Z),add_piece(Board,NewBoard,T,Z),
+	check_triple(NewBoard,Z,T,NewBoard2),another_player(T,T1),play(NewBoard2,T1).
+play(Board,T):- write('Incorrect, this position is not available'),nl,play(Board,T).
 
-new_game():- play(['e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e'],'T').
+new_game():- play(['e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e'],'a').
