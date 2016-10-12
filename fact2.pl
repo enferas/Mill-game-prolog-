a09909c2aa2caa6c1394fbfaab8f5296d55fd0f0
anasbenamou <good1anas@gmail.com>
2016-10-12 01:53:42 +0100

    Update fact2.pl

--- fact2.pl
+++ fact2.pl
@@ -1,3 +1,76 @@
+%permission to retractt
+:- dynamic board/1.
+:- retractall(lisst(_)).
+:- assert(lisst([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])). 
+%the list 
+
+% Positions
+% the first row
+position(1,1).
+position(1,4).
+position(1,7).
+
+%the sesecond row
+position(2,2).
+position(2,4).
+position(2,6).
+
+%the third row
+position(3,3).
+position(3,4).
+position(3,5).
+
+%the fourth row
+position(4,1).
+position(4,2).
+position(4,3).
+position(4,5).
+position(4,6).
+position(4,7).
+
+%the fifth row
+position(5,3).
+position(5,4).
+position(5,5).
+
+%the sixth row
+position(6,2).
+position(6,4).
+position(6,6).
+
+%the seventh row
+position(7,1).
+position(7,4).
+position(7,7).
+
+%convert from two dimentions to one dimention
+dim(1,1,1).
+dim(1,2,2).
+dim(1,3,3).
+dim(2,2,4).
+dim(2,4,5).
+dim(2,6,6).
+dim(3,3,7).
+dim(3,4,8).
+dim(3,5,9).
+dim(4,1,10).
+dim(4,2,11).
+dim(4,3,12).
+dim(4,5,13).
+dim(4,6,14).
+dim(4,7,15).
+dim(5,3,16).
+dim(5,4,17).
+dim(5,5,18).
+dim(6,2,19).
+dim(6,4,20).
+dim(6,6,21).
+dim(7,1,22).
+dim(7,4,23).
+dim(7,7,24).
+:- dynamic board/1.
+:- retractall(board(_)).
+:- assert(board([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])). 
 
 %Check if the player has new score or not
 score(Board,X,Y,T):- X=:=1, Y=:=1, Board = [T,T,T,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_].
@@ -49,8 +122,6 @@ score(Board,X,Y,T):- X=:=7, Y=:=4, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,_,
 score(Board,X,Y,T):- X=:=7, Y=:=7, Board = [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,T,T,T].
 score(Board,X,Y,T):- X=:=7, Y=:=7, Board = [_,_,T,_,_,_,_,_,_,_,_,_,_,_,T,_,_,_,_,_,_,_,_,T].
 
-rockit([_|L],[P|L],P,1).
-rockit([X|L],[X|L1],P,N):-N1 is N-1,rockit(L,L1,P,N1).
 
 
 %check if the player can make a move ot not
@@ -136,6 +207,11 @@ O--O--O     O--O--O
 O--------O--------O
 */
 
+fusion([X],[Y],[X]):- X > Y .
+fusion([X],[Y],[Y]):- X =< Y .
+fusion([X|L1],[Y|L2],[X|L3]):- X > Y ,fusion(L1,L2,L3).
+fusion([X|L1],[Y|L2],[Y|L3]):- X =< Y ,fusion(L1,L2,L3).
+
 print_element([X|_],1):-write(X).
 print_element([_|Y],N):-N1 is N-1,print_element(Y,N1).
 
@@ -157,9 +233,11 @@ print_board(Board):-
 
     available_position([X|_],1):- X=:=0.
     available_position([_|Y],N):-N1 is N-1,available_position(Y,N1).
+
     check(Board,Z):-available_position(Board,Z).
-     
-    play(Board,P,Z):-check(Board,Z),rockit(Board,B,P,Z),print_board(B).
+    rockit([_|L],[P|L],P,1).
+    rockit([X|L],[X|L1],P,N):-N1 is N-1,rockit(L,L1,P,N1).
+    play(Board,P,Z):-check(Board,Z),rockit(Board,B,P,Z),lisst(A),fusion(A,B,C),asserta(lisst(C)),print_board(C).
+
 
     
-
